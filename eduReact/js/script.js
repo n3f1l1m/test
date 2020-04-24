2e8b8607784290a829281d9b8371df1941df2bea
Gevorg <kirakosyangev@yahoo.com>
2020-04-24 07:41:56 +0400

    React edu started

---
+++ eduReact/js/script.js
@@ -0,0 +1,10 @@
+class UserAccessBtn extends React.Component{
+    render() {
+        return (
+            <div>
+                <button>Access</button>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<UserAccessBtn></UserAccessBtn>, document.getElementById('root'));
